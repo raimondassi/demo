c510aaa2f28b350142185232a5db4895c1691d2c
raimondassi <raimondassiup@gmail.com>
2021-07-05 18:35:15 +0300

    naujas komitas

---
+++ src/main/java/DumpFile.java
@@ -0,0 +1,8 @@
+public class DumpFile {
+    public static void main(String[] args) {
+        System.out.println("demo");
+        System.out.println("nauajas pakeitimas");
+        System.out.println("dar vienas pakeitimas");
+
+    }
+}
